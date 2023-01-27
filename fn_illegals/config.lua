aca9326075ca6e584f2c809469353478b777e12d
Guudz <Kilian1702@outlook.com>
2023-01-27 17:28:19 +0100

    Initial commit

---
+++ fn_illegals/config.lua
@@ -0,0 +1,33 @@
+Config = {}
+
+ 
+
+Config.MarkerColor = {r = 0, g = 200, b = 204}
+
+Config.JobName = "sheriff" -- required  job
+
+Config.Bonus = 25 -- money bonus
+
+
+
+Config.CountMoney = 75
+
+Config.BonusMoney = 25
+
+
+
+Config.Items = {"weed", "coke", "meth", "opium"} -- List of the illegal items (do not add weapons!)
+
+
+
+Config.Zones = { 
+
+    {type = 22, x = -441.81, y = 5987.37, z = 31.71}, -- script zones
+    {type = 22, x = -456.70, y = 5980.86, z = 31.42} -- script zones
+
+}
+
+
+
+Config.Locale = 'en'
+
---
+++ fn_illegals/locales/en.lua
@@ -0,0 +1,11 @@
+Locales['en'] = {
+
+    ["marker_do_txt"] = "~INPUT_TALK~ To Open The Illegal Menu",
+
+    ["not_items_notify"] = "You don't have coke / weed / opium / meth",
+
+    ["dialog_title"] = "How many ? ",
+
+    ["amount_invalid"] = "Invalid amount",
+
+}
